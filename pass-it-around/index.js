92504f61fb0c38c83c68f861624275773008c4cd
Alejandro Rojas <kmachappy@pm.me>
2022-04-02 20:18:27 -0700

    completed 99 bottles

--- pass-it-around/index.js
+++ pass-it-around/index.js
@@ -5,14 +5,23 @@ const app = express()
 const port = 3000
 
 
-app.get("/", (req,res)=>{
-    res.send(`99 Bottles of beer on the wall
+app.get("/:number_of_bottles", (req,res)=>{
+    const bottles = parseInt(req.params.number_of_bottles)
+    const newBottles = bottles - 1
     
+    if(newBottles !=0 ){
+    res.send(`${req.params.number_of_bottles} Bottles of beer on the wall
     <br/>
     <br/>
-    <a href="/">take one down, pass it around</a>
-`)
-})
+    <a href="/${newBottles}">take one down, pass it around</a>
+    `)}else{
+    res.send(`${req.params.number_of_bottles} Bottles of beer on the wall
+    <br/>
+    <br/>
+    <a href="/99">Took all down, restart!</a>
+    `)}
+
+})//event listener?
 
 
 
